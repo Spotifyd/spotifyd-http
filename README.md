7d81fea9b91fa8bb4370a947d3d31b543e3de0cc
Simon Persson <simon@flaskpost.org>
2016-08-31 21:08:42 +0200

    Add playlist modifications.

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ Returns a list of device ID/Name pairs.
 
 ### PUT /device_id/{play, pause, next, prev}
 Plays, pauses, skips, and returns to previous song.
+
+### {GET, PUT, POST} /device_id/tracks
+Gets, replaces, or appends tracks to the playlist. The `PUT` and `POST` take
+one or more `id` parameters. Example:
+```bash
+TRACKS="id=2BhU0Hl5OatWiCW93pE2b8&id=731OW49heGHCMrMOREHYlY&id=6zAPaRDoT99ThFtIXUJwhO"
+curl -X POST -d "$TRACKS" 127.0.0.1:6767/device_id/tracs
+```
--- src/web_server.rs
+++ src/web_server.rs
@@ -1,4 +1,4 @@
-use nickel::{Nickel, HttpRouter};
+use nickel::{Nickel, HttpRouter, FormBody};
 use nickel::status::StatusCode;
 
 use librespot::spirc::SpircManager;
@@ -17,10 +17,10 @@ pub fn run(spirc: SpircManager) {
     server.get("/devices",
                middleware!(spirc_device_list.devices().to_json()));
 
-    let spirc_tracks = spirc.clone();
+    let spirc_get_tracks = spirc.clone();
     server.get("/:device/:tracks",
                middleware! { |req, res|
-        if let Some(tracks) = spirc_tracks.device_tracks(req.param("device").unwrap()) {
+        if let Some(tracks) = spirc_get_tracks.device_tracks(req.param("device").unwrap()) {
             return res.send(tracks.ids
                                   .iter()
                                   .map(SpotifyId::to_base62)
@@ -30,6 +30,35 @@ pub fn run(spirc: SpircManager) {
         (StatusCode::NotFound, "No tracks for that device id.")
     });
 
+    let spirc_put_tracks = spirc.clone();
+    server.put("/:device/tracks",
+               middleware! { |req, res|
+        let device = req.param("device").unwrap().to_owned();
+        let form_data = try_with!(res, req.form_body());
+
+        if let Some(id_strs) = form_data.all("id") {
+            let tracks = id_strs.iter().map(|id| SpotifyId::from_base62(&*id));
+            spirc_put_tracks.clone().send_replace_queue(&*device, tracks);
+            StatusCode::Ok
+        } else {
+            StatusCode::BadRequest
+        }
+    });
+
+    let spirc_post_tracks = spirc.clone();
+    server.post("/:device/tracks",
+                middleware! { |req, res|
+        let device = req.param("device").unwrap().to_owned();
+        let form_data = try_with!(res, req.form_body());
+
+        if let Some(id_strs) = form_data.all("id") {
+            let tracks = id_strs.iter().map(|id| SpotifyId::from_base62(&*id));
+            spirc_post_tracks.clone().send_append_tracks(&*device, tracks);
+            StatusCode::Ok
+        } else {
+            StatusCode::BadRequest
+        }
+    });
 
     let spirc_cmd = spirc.clone();
     server.put("/:device/:cmd",
